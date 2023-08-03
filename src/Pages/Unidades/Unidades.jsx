b3667052f868c66856ff9f534b5b08ba53f0894a
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-08-03 17:34:20 -0300

    Mensagem pagina de Unidade

--- src/Pages/Unidades/Unidades.jsx
+++ src/Pages/Unidades/Unidades.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import "./Unidades.css"
 
 export default function Unidades () {
     return(
         <>
         <Navbar/>
-        <h1>teste Unidades</h1>
+        <h1>EM CONSTRUÇÃO</h1>
         </>
     )
 }
