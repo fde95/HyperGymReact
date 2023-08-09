1df00806c5257cbc0e79415db741246e3ed0d9e5
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-08-09 16:14:40 -0300

    Cirando informações da aba Unidade

--- src/Pages/Unidades/Unidades.jsx
+++ src/Pages/Unidades/Unidades.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/FooterBar"
+import { Container, Title, Subtitle } from "../../Components/Styles/ComponentsStyle";
 import "./Unidades.css"
 
 export default function Unidades () {
     return(
         <>
         <Navbar/>
-        <h1>EM CONSTRUÇÃO</h1>
+                <main>
+                <div className="buildingHero">
+                <Container>
+                        <div className="introducao">
+                            <Title>Mais que uma academia, um <span className="title__emphasis">Estilo de vida</span></Title>
+                            <Subtitle>Bem-vindo à Hyper: Aqui, a busca por um estilo de vida saudável vai além do exercício. Somos um espaço de transformação, onde corpo, mente e espírito se unem. Nossos instrutores são mentores comprometidos, guiando você rumo às suas metas. Oferecemos programas diversos, de treinos intensos a práticas revitalizantes. Nossa comunidade acolhedora compartilha conquistas e inspira. Na Hyper, não vendemos apenas uma academia, mas sim um compromisso com sua evolução completa. Junte-se a nós e abrace um estilo de vida vibrante e saudável!</Subtitle>
+                        </div>
+                    </Container>
+                </div>
+                </main>
+            
+        <Footer/>
         </>
     )
 }
